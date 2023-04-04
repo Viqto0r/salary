f7fef104b004a5fcb3112355ecc097df7736084b
Viqto0r <viqto0r@gmail.com>
2023-04-04 22:36:48 +0300

    add close btn for Output

--- src/components/Output/Output.tsx
+++ src/components/Output/Output.tsx
@@ -1,8 +1,22 @@
 import { FC, memo } from 'react'
 import styles from './Output.module.scss'
+import { useAppDispatch } from '../../app/hooks'
+import { toggleOppened } from '../../redux/slices/outputSlice'
 
 const Output: FC = () => {
-  return <div className={styles.container}></div>
+  const dispatch = useAppDispatch()
+  return (
+    <div className={styles.container}>
+      <div className={styles.output}>
+        <div
+          className={styles.closeBtn}
+          onClick={() => {
+            dispatch(toggleOppened())
+          }}
+        ></div>
+      </div>
+    </div>
+  )
 }
 
 export default memo(Output)
